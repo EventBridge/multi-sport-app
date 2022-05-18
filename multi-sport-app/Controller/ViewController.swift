0c0fa696f19af47c0ff7b8ac3502e0906e7b5aa1
Angusthj00 <100190524+Angusthj00@users.noreply.github.com>
2022-05-18 19:54:23 +1000

    uinew1 added

--- multi-sport-app/Controller/ViewController.swift
+++ multi-sport-app/Controller/ViewController.swift
@@ -17,39 +17,38 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
         
-        // Test fetch teams
-        NetworkService.shared.fetchTeams { [weak self] (result) in
-            switch result {
-            case.success(let teams):
-                self?.teams = teams
-                print("The decoded data is:\n \(self?.teams)")
-            case.failure(let error):
-                print("The error is: \(error.localizedDescription)")
-            }
-        }
-
-        // Test fetch players
-        NetworkService.shared.fetchPlayers(team: "1", season: "2021") { [weak self] (result) in
-            switch result {
-            case.success(let players):
-                self?.players = players
-                print("The decoded data is:\n \(self?.players)")
-            case.failure(let error):
-                print("The error is: \(error.localizedDescription)")
-            }
-        }
-        
-        NetworkService.shared.fetchArticles(query: "nba") { [weak self] (result) in
-            switch result {
-            case.success(let articles):
-                self?.articles = articles
-                print("The decoded data is:\n \(self?.articles)")
-            case.failure(let error):
-                print("The error is: \(error.localizedDescription)")
-            }
-        }
+//        // Test fetch teams
+//        NetworkService.shared.fetchTeams { [weak self] (result) in
+//            switch result {
+//            case.success(let teams):
+//                self?.teams = teams
+//                print("The decoded data is:\n \(self?.teams)")
+//            case.failure(let error):
+//                print("The error is: \(error.localizedDescription)")
+//            }
+//        }
+//
+//        // Test fetch players
+//        NetworkService.shared.fetchPlayers(team: "1", season: "2021") { [weak self] (result) in
+//            switch result {
+//            case.success(let players):
+//                self?.players = players
+//                print("The decoded data is:\n \(self?.players)")
+//            case.failure(let error):
+//                print("The error is: \(error.localizedDescription)")
+//            }
+//        }
+//
+//        NetworkService.shared.fetchArticles(query: "nba") { [weak self] (result) in
+//            switch result {
+//            case.success(let articles):
+//                self?.articles = articles
+//                print("The decoded data is:\n \(self?.articles)")
+//            case.failure(let error):
+//                print("The error is: \(error.localizedDescription)")
+//            }
+//        }
     }
-
 
 }
 
