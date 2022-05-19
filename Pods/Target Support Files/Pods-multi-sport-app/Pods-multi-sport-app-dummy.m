fec0fd08d4da78d610d9e534f1c1569f62dfbf68
EventBridge <vincentwhitetran@gmail.com>
2022-05-19 17:38:40 +1000

    initial

---
+++ Pods/Target Support Files/Pods-multi-sport-app/Pods-multi-sport-app-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_multi_sport_app : NSObject
+@end
+@implementation PodsDummy_Pods_multi_sport_app
+@end
--- multi-sport-app/Controller/TeamsVC.swift
+++ multi-sport-app/Controller/TeamsVC.swift
@@ -8,7 +8,52 @@
 import UIKit
 
 class TeamsVC: UIViewController {
+    @IBOutlet weak var teamCollectionView: UICollectionView!
+    
+    var teams: [Team] = []
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        teamCollectionView.delegate = self
+        teamCollectionView.dataSource = self
+        //teamCollectionView.collectionViewLayout = UICollectionViewLayout()
+        
+        registerCells()
+        
+        // fetch teams
+        NetworkService.shared.fetchTeams { [weak self] (result) in
+            switch result {
+            case.success(let teams):
+                self?.teams = teams
+                //print("The decoded data is:\n \(self?.teams)")
+                self?.teamCollectionView.reloadData()
+            case.failure(let error):
+                print("The error is: \(error.localizedDescription)")
+            }
+        }
+        
+        
+    }
+    
+    private func registerCells() {
+        teamCollectionView.register(UINib(nibName: TeamCollectionViewCell.identifier, bundle: nil), forCellWithReuseIdentifier: TeamCollectionViewCell.identifier)
+    }
+}
+
+extension TeamsVC: UICollectionViewDelegate, UICollectionViewDataSource {
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return teams.count
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: TeamCollectionViewCell.identifier, for: indexPath) as! TeamCollectionViewCell
+        cell.setup(team: teams[indexPath.row])
+        return cell
+    }
+}
+
+extension TeamsVC: UICollectionViewDelegateFlowLayout {
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        return CGSize(width: 128, height: 200)
     }
 }
