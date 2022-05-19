22873e568683ef1180ef8cda32d47af297e3c808
Angusthj00 <100190524+Angusthj00@users.noreply.github.com>
2022-05-19 19:39:29 +1000

    add the players view page

---
+++ multi-sport-app/CustomViews/CollectionViewCells/PlayerCollectionViewCell/PlayerCollectionViewCell.swift
@@ -0,0 +1,25 @@
+//
+//  PlayerCollectionViewCell.swift
+//  multi-sport-app
+//
+//  Created by ubisson on 19/05/2022.
+//
+
+import UIKit
+
+class PlayerCollectionViewCell: UICollectionViewCell {
+    
+    static let identifier = "PlayerCollectionViewCell"
+
+    
+    @IBOutlet weak var playerNameLabel: UILabel!
+    @IBOutlet weak var positionLabel: UILabel!
+    
+    func setup(player: Player) {
+        playerNameLabel.text = player.firstname! + player.lastname!
+        positionLabel.text = player.leagues?.standard?.pos
+    }
+    
+
+
+}
